bdb8ade9586ac74123e961357a6f6e06a237efb1
NishKrishnan <nishkrishnan7@gmail.com>
2015-05-12 22:30:35 -0400

    shit compiles now at least and added a testbench file

--- mainMem.v
+++ mainMem.v
@@ -6,28 +6,37 @@ parameter DATA_SIZE 	= 32;
 parameter MEM_SIZE 	= 1048578; // 1MB
 parameter MEM_WIDTH 	= 8;
 
-input clk, wren;
+input clk, wren, en;
 input [0:ADDRESS_SIZE-1] 	addr;
-input [0:DATA_SIZE-1] 		data_in;
+input [0:DATA_SIZE-1] 		d_in;
 input [0:ACCESS_SIZE-1] 	acc_size;
 
-output [0:DATA_SIZE-1] 		data_out;
+output [0:DATA_SIZE-1] 		d_out;
 output 				busy;
 
 reg [0:DATA_SIZE-1] 		mem_block [0:MEM_SIZE-1];
 
+integer i;
+
+initial begin
+	$display ("Initialize memory to zero");
+
+	for (i = 0; i < MEM_SIZE; i = i + 1) begin
+		mem_block[i] = 0;
+	end
+end
 
 always @ (posedge clk) begin
 		
 end
 
-case (acc_size)
-	00: 
-	01:
-	10:
-	11: 
-	default:
-endcase
+/*case (acc_size)
+	//2'b00: 
+	//2'b01:
+	//2'b10:
+	//2'b11: 
+	//default:
+endcase*/
 
 
 endmodule
