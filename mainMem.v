151bef00c5f0d7eae9f5fdba72afbf628b891a8b
Michaela Farova <michaela@mfaro.com>
2015-05-14 16:02:43 -0400

    Small changes to code

--- mainMem.v
+++ mainMem.v
@@ -13,7 +13,7 @@ module mainMem (clk, addr, d_in, d_out, acc_size, wren, busy, en);
 	input [0:DATA_SIZE-1] 		d_in;
 	input [0:ACCESS_SIZE-1] 	acc_size;
 
-	output reg[0:DATA_SIZE-1] 		d_out;
+	output [0:DATA_SIZE-1] 		d_out;
 	output 				busy;
 
 	reg [0:MEM_WIDTH-1] 		mem_block [0:MEM_SIZE-1];
@@ -37,36 +37,38 @@ module mainMem (clk, addr, d_in, d_out, acc_size, wren, busy, en);
 	// control signals
 	assign valid_addr = addr > START_ADDRESS && mem_index < MEM_SIZE;
 	
+	assign busy = en && valid_addr && word_counter < total_word;
+	
 	// Write data
 	always @ (posedge clk) begin
-		if(en && wren && valid_addr && word_counter < total_word) begin
+		if(busy && wren) begin
 			mem_block[mem_index] = d_in[0:7];
 			mem_block[mem_index+1] = d_in[8:15];
 			mem_block[mem_index+2] = d_in[16:23];
 			mem_block[mem_index+3] = d_in[24:31];
-		end
-		
-		if(word_counter == total_word) begin
-			word_counter = 0;
+			word_counter = word_counter + 1;
 		end
 	end
 	
 
 	// Read data
 	always @ (negedge clk) begin
-		if(en && !wren && valid_addr && word_counter < total_word) begin
+		if(busy && !wren) begin
 			d_out = { mem_block[mem_index],
 				mem_block[mem_index+1],
 				mem_block[mem_index+2],
 				mem_block[mem_index+3] };
 				
 			word_counter = word_counter + 1;
-		end	
-		
-		if(word_counter == total_word) begin
-			word_counter = 0;
 		end
 	end
+	
+	always @ (word_counter) begin
+		if(word_counter == total_word) begin
+				word_counter = 0;
+				
+		end
+	end 
 
 	always @ (acc_size) begin
 		case (acc_size)
