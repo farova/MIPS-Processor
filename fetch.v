412717c37175f005bb52e2725d66b86829540cab
Nishad Krishnan <Nish@Nishads-MacBook-Pro.local>
2015-05-26 00:13:38 -0400

    created the test bench and it works currently for sumarray except for one little bug where the 1st instruction is printed twice

--- fetch.v
+++ fetch.v
@@ -1,7 +1,7 @@
-module Fetch(clk, stall, pc_out, rw, acc_size_out);
+module fetch(clk, stall, pc_out, rw, acc_size_out);
 
 input clk, stall;
-output[0:31] pc_out;
+output reg[0:31] pc_out;
 output rw;
 output[0:1] acc_size_out;
 
@@ -10,17 +10,16 @@ reg[0:1]acc_size;
 
 
 initial begin
-	pc = 32'h80020000;
-	acc_size = 0;
+	pc_out <= 32'h80020000;
+	acc_size <= 0;
 end
 
-assign pc_out = pc;
 assign acc_size_out = 2'b00;
 
 
 always @(posedge clk) begin
 	if (!stall) begin
-		pc <= pc + 4;
+		pc_out <= pc_out + 4;
 	end
 end
 
